7ebfa65feae049df2555f409900c972bbadc81bd
Anshu Kulshrestha <anshu48k@gmail.com>
2024-08-19 18:07:49 +0530

    spring security

---
+++ section3/src/main/java/org/eazybank/controller/LoansController.java
@@ -0,0 +1,16 @@
+package org.eazybank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class LoansController {
+
+    @GetMapping("/myLoans")
+    public  String getLoansDetails () {
+        return "Here are the loans details from the DB";
+    }
+
+}
